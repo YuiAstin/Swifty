336802afcecc322402e6622b6e88af2f3b1f5b76
YuiAstin <48943936+YuiAstin@users.noreply.github.com>
2019-11-29 23:30:22 +0700

    update BUS functions for client
    update:
    - startgame
    - ranking list
    - player list

--- swifty-client/src/BUS/BUS.java
+++ swifty-client/src/BUS/BUS.java
@@ -1,8 +1,10 @@
 package BUS;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import main.*;
@@ -10,22 +12,82 @@ import main.*;
 
 public class BUS {
 
-	public static void sendNumber(String a) {
+	public static int sendNumber(String a) {
 		// TODO Auto-generated method stub
 		try {
 			String result ="{\n"
 				    +"\"Type\": \"MatchUpdate\",\n"
-					+"\"player ID\": "+main.Client.player.getString("player ID")+",\n"
+					+"\"player ID\": "+Client.player.getInt("player ID")+",\n"
 				    +"\"fieldUpdate\": "+a+"\n"
 				+"}";
-			DataOutputStream dos = new DataOutputStream(main.Client.socket.getOutputStream());
+			DataOutputStream dos = new DataOutputStream(Client.socket.getOutputStream());
 			dos.writeUTF(result);
-			
-		} catch (JSONException |IOException e) {
+			DataInputStream dis = new DataInputStream(Client.socket.getInputStream());
+			result = dis.readUTF();
+			JSONObject obj = new JSONObject(result);
+			return obj.getInt("fieldUpdate");
+		} catch (JSONException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return 0;
 		}
 		
+	}
+	
+	public static JSONArray get_rankingList()
+	{
+		
+		try {
+			String result ="{\"Type\": \"Ranking Request\"}";
+			DataOutputStream dos = new DataOutputStream(Client.socket.getOutputStream());
+			dos.writeUTF(result);
+			DataInputStream dis = new DataInputStream(Client.socket.getInputStream());
+			result = dis.readUTF();
+			JSONObject obj = new JSONObject(result);
+			return obj.getJSONArray("Player");
+		} catch (JSONException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static JSONArray get_playerList()
+	{
+		
+		try {
+			String result ="{\"Type\": \"Player List Request\"}";
+			DataOutputStream dos = new DataOutputStream(Client.socket.getOutputStream());
+			dos.writeUTF(result);
+			DataInputStream dis = new DataInputStream(Client.socket.getInputStream());
+			result = dis.readUTF();
+			JSONObject obj = new JSONObject(result);
+			return obj.getJSONArray("Player");
+		} catch (JSONException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static JSONObject startGame()
+	{
+		try {
+			String result ="{\n" + 
+					"    \"Type\": \"Create room\",\n" + 
+					"    \"player ID\": "+Client.player.getInt("player ID")+"\n" + 					 
+					"}";
+			DataOutputStream dos = new DataOutputStream(Client.socket.getOutputStream());
+			dos.writeUTF(result);
+			DataInputStream dis = new DataInputStream(Client.socket.getInputStream());
+			result = dis.readUTF();
+			JSONObject obj = new JSONObject(result);
+			return obj;
+		} catch (JSONException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 }
